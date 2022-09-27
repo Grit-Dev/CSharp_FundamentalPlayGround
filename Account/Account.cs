b4b5f769579348da01c0e1e17df165c90661a168
Paul McGinley <paul.mcginley@eso.com>
2022-09-27 16:03:01 +0100

    [Account/files] Two files created. Account and Customer

---
+++ Account/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalPlayGround.Account
+{
+    public class Account
+    {
+        //Field
+        private string _AccountNumber = ""; 
+        
+        //Properties
+        public string AccountNumber
+        {
+            get { return _AccountNumber; }
+            set { _AccountNumber = value; }
+
+        }
+
+        public void TheAccountNumber()
+        {
+            Console.WriteLine($"{AccountNumber}");
+        }
+
+    }
+}
---
+++ Account/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalPlayGround.Account
+{
+    public class Customer
+    {
+        private int _CustomerID; 
+        private string _Name = ""; 
+        private string _Email = "";
+
+        public int CutomerID
+        {
+            get { return _CustomerID; }
+            set { _CustomerID = value; }
+        }
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; }
+        }
+
+    }
+}
